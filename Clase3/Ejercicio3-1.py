b0ae1eb0aa7bf46654ed1f89bb39e7a34431c097
Santiago Daglio <santidaglio@gmail.com>
2022-04-12 23:51:23 -0300

    Ejercicio 1 clase 3

---
+++ Clase3/Ejercicio3-1.py
@@ -0,0 +1,24 @@
+
+nombre: str = 'Santiago'
+apellido: str = "Rotson"
+edad: int = 29
+email: str = 'santi@sanndag.com'
+telefono: int = 119395353
+casado: bool = True
+conHijos: bool = False
+listaAmigos: list = ['Diego', 'Ramiro', 'Damian']
+peliculasVistas = {
+    "1": "El Padrino",
+    "2": "Goodfellas",
+    "3": "El Club de la Pelea"
+}
+
+print(nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(conHijos)
+print(listaAmigos)
+print(peliculasVistas)
