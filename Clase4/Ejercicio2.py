7574c645bbcbc63fa660dd9b57e4ccb8842de842
Santiago Daglio <santidaglio@gmail.com>
2022-04-15 16:16:08 -0300

    Ejercicio2

--- Clase4/Ejercicio2.py
+++ Clase4/Ejercicio2.py
@@ -2,7 +2,18 @@
 
 #Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8]
-for impar in numeros:
-    if impar % 2 == 1:
-        print([impar])
+numero_inicial: int = int(input("Ingrese el primer numero: "))
+numero_final: int = int(input("Ingrese el segundo numero: "))
+numeros_impares: [int] = []
+
+while numero_final <= numero_inicial:
+    numero_final: int = int(input("El segundo numero debe sermayor al primero. Ingrese otro numero: "))
+
+for i in range(numero_inicial, numero_final+1):
+    if(i % 2 == 1):
+        numeros_impares.append(i)
+
+print(f"Lista de numeros impares entre {numero_inicial} y {numero_final}: ")
+print(numeros_impares)
+
+    
