0236c0d8a3f7c0aa9ceb973181400741b348f079
Santiago Daglio <santidaglio@gmail.com>
2022-04-17 23:49:52 -0300

    Ejercicio 3

--- Clase5/Ejercicio3.py
+++ Clase5/Ejercicio3.py
@@ -2,7 +2,7 @@
 
 anio = int(input("Ingrese un año: "))
 
-if anio % 4 == 0: 
+if (anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0)): 
     print("El año ingresado es bisiesto")
 else: 
     print("El año ingresado no es bisiesto")
